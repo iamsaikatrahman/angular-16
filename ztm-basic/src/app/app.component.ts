626bcd8ee4a9c4e1edd7b41600271370e8f27ca5
Saikat Rahman <saikatrahmanofficial@gmail.com>
2024-04-28 00:41:41 +0600

    001 Interpolation

--- ztm-basic/src/app/app.component.ts
+++ ztm-basic/src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ztm-basic';
+  name = "Saikat"
+
+  getUserName(){
+    return this.name;
+  }
 }
