d964ec0e7eba22cf436bfea82a097347c739347f
Saikat Rahman <saikatrahmanofficial@gmail.com>
2024-04-27 12:48:41 +0600

    83

--- cmp-databinging/src/app/server-element/server-element.component.ts
+++ cmp-databinging/src/app/server-element/server-element.component.ts
@@ -1,19 +1,23 @@
-import { Component, Input, OnChanges, OnInit, SimpleChange } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, OnChanges, OnInit, SimpleChange, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
   styleUrls: ['./server-element.component.css']
 })
-export class ServerElementComponent implements OnInit{
+export class ServerElementComponent implements OnInit,AfterViewInit{
  @Input() element: any;
  @Input() name: any;
+
+ @ViewChild('heading') header: ElementRef | undefined;
+
  ngOnChanges(changes: SimpleChange){
   console.log('ngOnChanges called!');
   console.log(changes);
  }
  ngOnInit(){
   console.log('ngOnInit called!');
+  console.log('Text Content: ', + this.header?.nativeElement.textContent);
  }
  ngDoCheck(){
   console.log('ngDoCheck called!');
@@ -26,6 +30,7 @@ export class ServerElementComponent implements OnInit{
  }
  ngAfterViewInit(){
   console.log('ngAfterViewInit called!');
+  console.log('Text Content: ', + this.header?.nativeElement.textContent);
  }
  ngAfterViewChecked(){
   console.log('ngAfterViewChecked called!');
