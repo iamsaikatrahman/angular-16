822abc1e5f8a648196a6b6e4000d7a981c48a490
Saikat Rahman <saikatrahmanofficial@gmail.com>
2024-05-04 17:58:56 +0600

    Transforming Content

--- ztm-basic/src/app/app.component.ts
+++ ztm-basic/src/app/app.component.ts
@@ -6,10 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-
+  name = "saikat rahman"
   imgUrl = "https://picsum.photos/id/237/200/300"
-
-  name = "Saikat"
+  images = [
+    'https://picsum.photos/id/236/200/300',
+    'https://picsum.photos/id/237/200/300',
+    'https://picsum.photos/id/238/200/300'
+  ];
+  currentDate = new Date()
+  cost = 2000
+  temperature = 25.3
+  pizza = {
+    topping: ['peperoni', 'bacon'],
+    size: 'large'
+  }
+  blueClass = false;
+  fontSize = 48;
 
   getUserName(){
     return this.name;
