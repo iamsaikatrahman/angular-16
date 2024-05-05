61d953372b030d49432b779e69ad28bf1f800ddc
Saikat Rahman <saikatrahmanofficial@gmail.com>
2024-05-05 08:59:12 +0600

    019 Opening the Modal

--- clips/src/app/nav/nav.component.ts
+++ clips/src/app/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ModalService } from '../services/modal.service';
 
 @Component({
   selector: 'app-nav',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class NavComponent {
 
+  constructor(public modal: ModalService){
+
+  }
+  openModal($e: Event){
+    $e.preventDefault(); // Users will not unexpectedly be redirected to a different page.
+    this.modal.toggleModal();
+  }
 }
